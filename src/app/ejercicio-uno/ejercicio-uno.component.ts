3e0e1cd985f21160b8c7bd77c8cea79a5c6dd113
Job <pulachecarrenojob837@gmail.com>
2024-11-17 04:03:55 -0500

    creatin components

---
+++ src/app/ejercicio-uno/ejercicio-uno.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicio-uno',
+  standalone: true,
+  imports: [],
+  templateUrl: './ejercicio-uno.component.html',
+  styleUrl: './ejercicio-uno.component.css'
+})
+export class EjercicioUnoComponent {
+
+}
