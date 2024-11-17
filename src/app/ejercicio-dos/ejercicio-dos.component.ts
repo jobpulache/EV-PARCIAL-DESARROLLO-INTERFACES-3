421381203f88e140c763ce63400ee3bed24e277f
Job <pulachecarrenojob837@gmail.com>
2024-11-17 10:10:05 -0500

    Add styles to  interfaces

--- src/app/ejercicio-dos/ejercicio-dos.component.ts
+++ src/app/ejercicio-dos/ejercicio-dos.component.ts
@@ -11,27 +11,26 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './ejercicio-dos.component.css'
 })
 export class EjercicioDosComponent {
-  montoPrestamo: number=0
-  cuotas:number=0
-  tasaInteres: number = 0
-  montoTotal: number=0;
-  montoPorCuota:number=0;
+  montoPrestamo: number = 0;
+  cuotas: number = 0;
+  tasaInteres: number = 0;
+  montoTotal: number = 0;
+  montoPorCuota: number = 0;
 
-  calcularPrestamo():void{
-    if(this.montoPrestamo > 5000){
-      this.cuotas=3
-    }else if (this.montoPrestamo <1000){
+  calcularPrestamo(): void {
+    if (this.montoPrestamo > 5000) {
+      this.cuotas = 3;
+    } else if (this.montoPrestamo < 1000) {
       this.cuotas = 1;
-    }else  if(this.montoPrestamo >= 2000 && this.montoPrestamo <=3000){
-      this.cuotas=1;
-    }else{
-      this.cuotas=5;
+    } else if (this.montoPrestamo >= 2000 && this.montoPrestamo <= 3000) {
+      this.cuotas = 2;
+    } else {
+      this.cuotas = 5;
     }
-    this.tasaInteres = this.montoPrestamo <4000 ?  0.12: 0.10;
+
+    this.tasaInteres = this.montoPrestamo < 4000 ? 0.12 : 0.10;
 
     this.montoTotal = this.montoPrestamo * (1 + this.tasaInteres);
     this.montoPorCuota = this.montoTotal / this.cuotas;
   }
- 
 }
-
--- src/app/ejercicio-tres/ejercicio-tres.component.ts
+++ src/app/ejercicio-tres/ejercicio-tres.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ejercicio-tres',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ejercicio-tres.component.html',
   styleUrl: './ejercicio-tres.component.css'
 })
 export class EjercicioTresComponent {
-
+  anioNacimiento: number | null = null;
+  mensaje: string = '';
+  verificarDni(): void {
+    const anioActualy = new Date().getFullYear();
+    const edad = anioActualy - (this.anioNacimiento || 0);
+    if (this.anioNacimiento === null || isNaN(this.anioNacimiento)) {
+      this.mensaje = 'Igrese un número valido';
+    }else if(edad >=18){
+      this.mensaje = `Ve a sacar DNI. Tienes ${edad}`
+    }else{
+      this.mensaje = `No necesitas sacar DNI. Todavia tienes ${edad}`
+    }
+  }
 }
