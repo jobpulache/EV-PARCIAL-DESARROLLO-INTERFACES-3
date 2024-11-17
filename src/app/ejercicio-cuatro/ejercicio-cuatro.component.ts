a8598ef5caef2d3f043705d282adf758e1e5afa9
Job <pulachecarrenojob837@gmail.com>
2024-11-17 10:47:32 -0500

    finish excersice four

--- src/app/ejercicio-cuatro/ejercicio-cuatro.component.ts
+++ src/app/ejercicio-cuatro/ejercicio-cuatro.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ejercicio-cuatro',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './ejercicio-cuatro.component.html',
   styleUrl: './ejercicio-cuatro.component.css'
 })
 export class EjercicioCuatroComponent {
+  numerosPares: number[] = [];
+  constructor() {
+    this.obtenerNumerosPares();
+  }
+
+  obtenerNumerosPares() {
+    for (let num = 20; num >= 10; num--) {
+      if (num % 2 === 0)
+        this.numerosPares.push(num)
+    }
+  }
 
 }
